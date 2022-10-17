6516122224e32183eed72ddd1f47342ac4d62cb0
chang <changlee99@gmail.com>
2022-10-18 07:45:12 +0900

    256 batch working

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-python main.py --model ./configs/gpt3-small-256-lcw.json --steps_per_checkpoint 100 \
-    --gpu_ids device:GPU:0 device:GPU:1 
+python main.py --model ./configs/gpt3-small-256-lcw.json --steps_per_checkpoint 200 \
+    --prompt "./prompt.txt" \
+    --gpu_ids device:GPU:1 device:GPU:0 
